d80711abf9072e83cae9bd2444c71b46de78fb50
Gabe Koeb <gfkoeb@ncsu.edu>
2024-03-10 17:55:51 -0400

    Fix Typewriter on front page

--- app/components/Typewriter.tsx
+++ app/components/Typewriter.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect, use } from "react";
+import React, { useState, useEffect } from "react";
 
-const useTypewriter = (text: string, speed: number = 100): string => {
+const useTypewriter = (text: string, speed: number): string => {
   const [output, setOutput] = useState<string>("");
   let index = 0;
 
@@ -8,12 +8,20 @@ const useTypewriter = (text: string, speed: number = 100): string => {
     let i = 0;
     const interval = setInterval(() => {
       if (i < text.length) {
-        setOutput((prevText) => prevText + text.charAt(i));
+        // Check if the next character is a space or a letter
+        const nextChar = text.charAt(i);
+        const isSpaceOrLetter = /^[a-zA-Z\s]+$/.test(nextChar);
+
+        // Only update output if it's a space or a letter
+        if (isSpaceOrLetter) {
+          setOutput((prevText) => prevText + nextChar);
+        }
         i++;
       } else {
         clearInterval(interval);
       }
     }, speed);
+    return () => clearInterval(interval);
   }, [text, speed]);
 
   return output;
@@ -23,4 +31,5 @@ const Typewriter = ({ text, speed = 100 }: { text: string; speed: number }) => {
   const output = useTypewriter(text, speed);
   return <p>{output}</p>;
 };
+
 export default Typewriter;
--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,21 @@
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-
+import Typewriter from "./components/Typewriter";
 export default function Home() {
   const pathname = usePathname();
   return (
     <main className="flex flex-col p-24 items-center justify-center h-screen">
       <div className="container flex flex-col align-middle items-center justify-center">
-        <h1 className="text-center text-6xl">
+        <h2 className="text-center text-6xl">
           <Link
+            key="link"
+            href="/about" 
             style={{ color: "#bdada0" }}
-            className={`link ${pathname === "/about" ? "active" : ""}`}
-            href="/about">
-            Gameboards
+            className={`link ${pathname === "/about" ? "active" : ""}`}>
+            <Typewriter text="Welcome to Gameboards" speed={50} />
           </Link>
-        </h1>
+        </h2>
         <h2 className="text-center">Written by Gabriel Koeb</h2>
         <h2 className="text-center">Click above to enter</h2>
       </div>
