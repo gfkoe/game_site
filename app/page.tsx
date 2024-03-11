f1f467ef39d6b9351eb1be9f5b9feaeded2bc31e
Gabe Koeb <gfkoeb@ncsu.edu>
2024-03-11 15:09:50 -0400

    Updated Typewriter speed on main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,58 @@
 "use client";
 import Image from "next/image";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Typewriter from "./components/Typewriter";
 export default function Home() {
   const pathname = usePathname();
+  const [showWelcome, setShowWelcome] = useState(false);
+  const [showAuthor, setShowAuthor] = useState(false);
+  const [showClick, setShowClick] = useState(false);
+
+  useEffect(() => {
+    const welcomeTimeout = setTimeout(() => {
+      setShowWelcome(true);
+    }, 1000);
+    const authorTimeout = setTimeout(() => {
+      setShowAuthor(true);
+    }, 3000);
+    const clickTimeout = setTimeout(() => {
+      setShowClick(true);
+    }, 5000);
+
+    return () => {
+      clearTimeout(welcomeTimeout);
+      clearTimeout(authorTimeout);
+      clearTimeout(clickTimeout);
+    };
+  }, []);
   return (
     <main className="flex flex-col p-24 items-center justify-center h-screen">
       <div className="container flex flex-col align-middle items-center justify-center">
-        <h2 className="text-center text-6xl">
-          <Link
-            key="link"
-            href="/about" 
-            style={{ color: "#bdada0" }}
-            className={`link ${pathname === "/about" ? "active" : ""}`}>
-            <Typewriter text="Welcome to Gameboards" speed={50} />
-          </Link>
-        </h2>
-        <h2 className="text-center">Written by Gabriel Koeb</h2>
-        <h2 className="text-center">Click above to enter</h2>
+        {showWelcome && (
+          <h2 className="text-center text-6xl">
+            <Link
+              key="link"
+              href="/about"
+              style={{ color: "#bdada0" }}
+              className={`link ${pathname === "/about" ? "active" : ""}`}
+            >
+              <Typewriter text="Welcome to Gameboards" speed={50} />
+            </Link>
+          </h2>
+        )}
+        {showAuthor && (
+          <h2 className="text-center">
+            <Typewriter text="Written by Gabriel Koeb" speed={50} />
+          </h2>
+        )}
+
+        {showClick && (
+          <h2 className="text-center">
+            <Typewriter text="Click above to enter" speed={50} />
+          </h2>
+        )}
       </div>
     </main>
   );
