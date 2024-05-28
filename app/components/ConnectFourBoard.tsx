9f023f38020e585ee42052f4cd02fb656096c0a6
Gabe Koeb <gfkoeb@ncsu.edu>
2024-05-28 14:03:11 -0400

    Add from nvim

--- app/components/ConnectFourBoard.tsx
+++ app/components/ConnectFourBoard.tsx
@@ -1,10 +1,9 @@
 import react from "react";
 import { useState } from "react";
 
-
 export default function ConnectFourBoard() {
   const [board, setBoard] = useState<string[][]>(
-    Array.from({length: 7}, () => Array.from({length: 6}, () => " ")));
+    Array.from({ length: 7 }, () => Array.from({ length: 6 }, () => " ")),
+  );
   return <div></div>;
 }
-
