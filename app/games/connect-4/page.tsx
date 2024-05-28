c76d107f27e150ec3d65dfebe014d17c68db9dff
Gabe Koeb <gfkoeb@ncsu.edu>
2024-05-28 13:49:42 -0400

    Added page for Connect4

---
+++ app/games/connect-4/page.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+export default function Connect4() {
+  return (
+    <main className="flex flex-col items-center min-h-[90vh]">
+      <div></div>
+    </main>
+  );
+}
