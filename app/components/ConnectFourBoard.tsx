9567dbf0e09b638c8decd82d7c99786b06353e6c
gfkoeb <gfkoeb@ncsu.edu>
2024-06-09 23:08:28 -0400

    initial working conect 4 game

--- app/components/ConnectFourBoard.tsx
+++ app/components/ConnectFourBoard.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ConnectFourSquare from "./ConnectFourSquare";
 export default function ConnectFourBoard() {
-  const [board, setBoard] = useState<string[][]>(
-    Array.from({ length: 7 }, () => Array.from({ length: 6 }, () => ""))
-  );
-  console.log(board);
-  // const [board, setBoard] = useState([
-  //   ["", "", "", "", "", "", ""],
-  //   ["", "", "", "", "", "", ""],
-  //   ["", "", "", "", "", "", ""],
-  //   ["", "", "", "", "", "", ""],
-  //   ["", "", "", "", "", "", ""],
-  //   ["", "", "", "", "", "", ""],
-  // ]);
+  // const [board, setBoard] = useState<string[][]>(
+  //   Array.from({ length: 7 }, () => Array.from({ length: 6 }, () => ""))
+  // );
+  // console.log(board);
+  const [board, setBoard] = useState([
+    ["", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", ""],
+  ]);
   const [currentPlayer, setCurrentPlayer] = useState<string>("X");
   const [isWinner, setIsWinner] = useState<boolean>(false);
   const gameBoard = board.map((row, i) => {
@@ -34,11 +34,31 @@ export default function ConnectFourBoard() {
     });
   });
   return (
-    <div>
-      game here:
-      <div className="flex items-center justify-center text-center cf-Container">
-        {gameBoard}
+    <div className="main-Page-Container">
+      <br />
+      <div className="player-container">
+        <h1 className="text-center">Current Player: </h1>
+        {currentPlayer === "X" ? (
+          <div className="x-token"></div>
+        ) : (
+          <div className="o-token"></div>
+        )}
       </div>
+
+      <div className="cf-Container">{gameBoard}</div>
+      <br />
+      {isWinner ? (
+        <div className="player-container">
+          <h1>Winner: </h1>
+          {currentPlayer === "X" ? (
+            <div className="o-token"></div>
+          ) : (
+            <div className="x-token"></div>
+          )}
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
--- app/components/ConnectFourSquare.tsx
+++ app/components/ConnectFourSquare.tsx
@@ -15,8 +15,64 @@ export default function ConnectFourSquare(props: squareProps) {
       changeTurn();
     }
   }
-  function checkWinner(row: number, col: number, board: string[][]) : void {
-    throw new Error("Function not implemented.");
+  function checkWinner(row: number, col: number, board: string[][]): void {
+    // throw new Error("Function not implemented.");
+    let count = 1;
+    let checkArray = [
+      [-1, -1],
+      [0, -1],
+      [1, -1],
+      [1, 0],
+    ];
+    checkArray.forEach((element) => {
+      //
+      initialCheck(count, row, col, element[0], element[1], board);
+    });
+  }
+
+  function initialCheck(
+    count: number,
+    row: number,
+    col: number,
+    checkRow: number,
+    checkCol: number,
+    board: string[][]
+  ) {
+    let newCount = 1;
+    if (count === 4) {
+      props.setIsWinner(true);
+    }
+    let newRow = row + checkRow;
+    let newCol = col + checkCol;
+    if (newRow >= 0 && newRow < 6 && newCol >= 0 && newCol < 7) {
+      if (board[newRow][newCol] === props.currentPlayer) {
+        count++;
+        initialCheck(count, newRow, newCol, checkRow, checkCol, board);
+      } else {
+        secondaryCheck(newCount, row, col, checkRow, checkCol, board);
+      }
+    }
+  }
+  function secondaryCheck(
+    count: number,
+    row: number,
+    col: number,
+    checkRow: number,
+    checkCol: number,
+    board: string[][]
+  ) {
+    if (count === 4) {
+      props.setIsWinner(true);
+    }
+    let newRow = row - checkRow;
+    let newCol = col - checkCol;
+
+    if (newRow >= 0 && newRow < 6 && newCol >= 0 && newCol < 7) {
+      if (board[newRow][newCol] === props.currentPlayer) {
+        count++;
+        secondaryCheck(count, newRow, newCol, checkRow, checkCol, board);
+      }
+    }
   }
   function changeTurn(): void {
     let tempBoard: string[][] = [...props.board];
@@ -36,5 +92,19 @@ export default function ConnectFourSquare(props: squareProps) {
     }
   }
 
-  return <div>test</div>;
+  return (
+    <div className="c4-item" onClick={handleClick}>
+      {props.board[props.i][props.j] === "X" ? (
+        <div className="x-token"></div>
+      ) : (
+        <>
+          {props.board[props.i][props.j] === "O" ? (
+            <div className="o-token"></div>
+          ) : (
+            <div></div>
+          )}
+        </>
+      )}
+    </div>
+  );
 }
